a868b0aacd93aa6faaa4e54383a940b46f3624c0
Seo-Faper <dswhdcks@gmail.com>
2019-05-02 20:46:46 +0900

    실시간 소캣 통신

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname+'/public'));
 
-app.listen(3000,function(){
-  console.log('server On!');
-})
+app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+var data={count:0};
+
+app.get('/hello', function(req,res){
+ res.render("hello", {name:req.query.nameQuery});
+});
+// 3
+
+app.get("/hello/:nameParam", function(req,res){
+ res.render("hello", {name:req.params.nameParam});
+});
+
+app.get('/', function (req,res) {
+  data.count++;
+  res.render('my_first_ejs',data);
+});
+app.get('/reset', function (req,res) {
+  data.count=0;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/count', function (req,res) {
+  if(req.query.count) data.count=req.query.count;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/:num', function (req,res) {
+  data.count=req.params.num;
+  res.render('my_first_ejs',data);
+});
+
+app.listen(3000, function(){
+  console.log('Server On!');
+});
